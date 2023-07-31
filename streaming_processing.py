e7a1adf046fbf5346e53b9b7814a9228dd816ab6
Hamza Mohammed <47981812+Hmohammed2@users.noreply.github.com>
2023-07-31 05:27:14 +0100

    Update streaming_processing.py

--- streaming_processing.py
+++ streaming_processing.py
@@ -1,92 +1,97 @@
-from pyspark.sql import SparkSession
-import multiprocessing
-import pyspark
 import os
 import yaml
-from pyspark.sql.functions import col
-from pyspark.sql.functions import *
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StringType, IntegerType, BinaryType
+from pyspark.sql.functions import from_json, col
 
-# Adding the packages required to get data from S3 and write data to Cassandra
-os.environ["PYSPARK_SUBMIT_ARGS"] = "--packages com.amazonaws:aws-java-sdk-s3:1.12.196,org.apache.hadoop:hadoop-aws:3.3.1,com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 pyspark-shell"
+# Download spark sql kakfa package from Maven repository and submit to PySpark at runtime. 
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1,org.postgresql:postgresql:42.5.0 pyspark-shell'
+# specify the topic we want to stream data from.
+kafka_topic_name = "Pinterest_data"
+# Specify your Kafka server to read data from.
+kafka_bootstrap_servers = 'localhost:9092'
 
-with open('config/s3_creds.yaml','r') as f:
-    s3_creds = yaml.safe_load(f)
-    print(type(s3_creds))
+with open('config/postgresql_creds.yaml','r') as f:
+    psql_creds = yaml.safe_load(f)
+    print(type(psql_creds))
 
-cfg = (
-    pyspark.SparkConf()
-    # Setting the master to run locally and with the maximum amount of cpu coresfor multiprocessing.
-    .setMaster(f"local[{multiprocessing.cpu_count()}]")
-    # Setting application name
-    .setAppName("Pinterest_batch_processing")
-    # Setting config value via string
-    .set("spark.eventLog.enabled", False)
-    # Setting environment variables for executors to use
-    .setExecutorEnv(pairs=[("VAR3", "value3"), ("VAR4", "value4")])
-    # Setting memory if this setting was not set previously
-    .setIfMissing("spark.executor.memory", "1g")
+spark = SparkSession \
+        .builder \
+        .appName("KafkaStreaming ") \
+        .getOrCreate()
+
+# Only display Error messages in the console.
+spark.sparkContext.setLogLevel("ERROR")
+
+# construct schema for streaming dataframe object
+json_schema = StructType()\
+        .add("category", StringType())\
+        .add("index", IntegerType())\
+        .add("unique_id", StringType())\
+        .add("title", StringType())\
+        .add("description", StringType())\
+        .add("follower_count", StringType())\
+        .add("tag_list", StringType())\
+        .add("is_image_or_video", StringType())\
+        .add("image_src", StringType())\
+        .add("downloaded", IntegerType())\
+        .add("save_location", StringType())   
+
+
+# Construct a streaming DataFrame that reads from topic
+stream_df = spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", kafka_bootstrap_servers) \
+        .option("subscribe", kafka_topic_name) \
+        .option("startingOffsets", "earliest") \
+        .load()
+
+# Select the value part of the kafka message and cast it to a string.
+stream_df = stream_df\
+    .withColumn("value", from_json(stream_df["value"].cast("string"), json_schema))\
+    .select(col("value.*"))
+
+# Clean the data before writing to stream
+stream_df = stream_df.select(
+                'unique_id',
+                'index',
+                'title',
+                'category',
+                'description',
+                'follower_count',
+                'tag_list',
+                'is_image_or_video',
+                'image_src',
+                'downloaded',
+                'save_location'
 )
 
-sc = pyspark.SparkContext(conf=cfg)
+stream_df = stream_df.withColumnRenamed("index", "index_id")
+# replace error or empty data with Nones
 
-access_key = s3_creds['aws_access_key_id']
-secret_access_key = s3_creds['aws_secret_access_key']
-
-hadoopConf = sc._jsc.hadoopConfiguration()
-hadoopConf.set('fs.s3a.access.key', access_key)
-hadoopConf.set('fs.s3a.secret.key', secret_access_key)
-hadoopConf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') 
-
-session = SparkSession(sc).Builder().getOrCreate()
-
-def read_from_s3(session):
-    """Read data from s3 bucket"""
-    # Read from the S3 bucket
-    df = session.read.option("multiline","true").json('s3a://pinterest-data-bucket/*') 
-
-    return df
-
-def transform_data(df):
-        """Do transformation on data using pyspark"""
-
-        # replace error or empty data with Nones
-        df = df.replace({'User Info Error': None}, subset = ['follower_count']) \
+stream_df = stream_df.replace({'User Info Error': None}, subset = ['follower_count']) \
                          .replace({'No description available Story format': None}, subset = ['description']) \
                          .replace({'No description available': None}, subset = ['description']) \
                          .replace({'Image src error.': None}, subset = ['image_src'])\
                          .replace({'N,o, ,T,a,g,s, ,A,v,a,i,l,a,b,l,e': None}, subset = ['tag_list'])\
                          .replace({'Image src error.': None}, subset = ['image_src'])\
-                         .replace({"No Title Data Available": None}, subset = ['title']) \
-        
-        # Convert the unit to corresponding zeros
-        df = df.withColumn("follower_count", when(col('follower_count').like("%k"), regexp_replace('follower_count', 'k', '000')) \
-                        .when(col('follower_count').like("%M"), regexp_replace('follower_count', 'M', '000000'))\
-                        .cast("int"))
+                         .replace({"No Title Data Available": None}, subset = ['title'])
 
-        # Convert the type into int
-        df= df.withColumn("downloaded", df["downloaded"].cast("int")) \
-                        .withColumn("index", ddf["index"].cast("int")) 
-        
-        # Rename the column
-        df = df.withColumnRenamed("index", "index_id")
+def _write_to_postgres(stream_df, epoch_id)-> None:
+    
+    stream_df.write \
+        .mode('append') \
+        .format("jdbc") \
+        .option("url", psql_creds["URL"]) \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", psql_creds["DBTABLE"]) \
+        .option("user", psql_creds["USER"]) \
+        .option("password", psql_creds["PASSWORD"]) \
+        .save()
 
-        # reorder columns
-        df = df.select('unique_id',
-                                'index_id',
-                                'title',
-                                'category',
-                                'description',
-                                'follower_count',
-                                'tag_list',
-                                'is_image_or_video',
-                                'image_src',
-                                'downloaded',
-                                'save_location'
-                                )
-
-        df.show(10)
-
-
-if __name__ == "__main__":
-    data = read_from_s3(session)
-    transform_data(data)
+# outputting the messages to the console 
+stream_df.writeStream \
+    .foreachBatch(_write_to_postgres) \
+    .start() \
+    .awaitTermination()
